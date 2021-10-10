62ea0b9b037989e5845f4730933ab4859bc64534
George4788 <20221577@student.curtin.edu.au>
2021-10-11 04:12:19 +0800

    Commit

---
+++ app/src/main/java/com/example/madassignment2/RegisterStudent.java
@@ -0,0 +1,21 @@
+package com.example.madassignment2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Pattern;
+
+public class RegisterStudent extends AppCompatActivity {
+    private String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_student);
+
+        //Email Verification
+        Pattern pattern = Pattern.compile(regex);
+    }
+}
---
+++ app/src/main/java/com/example/madassignment2/Student.java
@@ -0,0 +1,9 @@
+package com.example.madassignment2;
+
+import android.graphics.Bitmap;
+
+public class Student {
+    private String fName, lName, email;
+    private int phoneNumber;
+    private Bitmap photo;
+}
